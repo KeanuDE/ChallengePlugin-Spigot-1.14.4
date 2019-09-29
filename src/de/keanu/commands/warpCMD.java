fe56eed8c9a2cdf848a408aa9f22f0230a0f4cad
Keanu <skyloxkeanu@gmail.com>
2019-09-29 20:28:52 +0200

    Kleinigkeiten..

--- src/de/keanu/commands/warpCMD.java
+++ src/de/keanu/commands/warpCMD.java
@@ -1,6 +1,7 @@
 package de.keanu.commands;
 
 import de.keanu.main.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,6 +17,7 @@ public class warpCMD implements CommandExecutor {
                 if(args[0].equalsIgnoreCase("set")) {
                     Main.warps.put(args[1],p.getLocation());
                     p.sendMessage("§aWarp-Punkt platziert!");
+                    Bukkit.broadcastMessage("§e" + p.getDisplayName() + "§7 hat den Warp-Punkt " + args[1] + " erstellt!");
                 }
                 else if(args[0].equalsIgnoreCase("tp")) {
                     p.teleport(Main.warps.get(args[1]));
